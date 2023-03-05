5cca397bfcea5ebd26da2896242d13dc2a8dc222
cprabhu <cprabhu@live.com>
2023-03-06 03:00:59 +0530

    props date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
-
+    let month=props.date.toLocaleString('en-us',{month:'long'});
+    let day=props.date.toLocaleString('en-us',{day:'2-digit'});
+    let year=props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+        </div>
       <div className="expense-item__description">{props.title}</div>
       <div className="expense-item__price">${props.amount}</div>
     </div>
